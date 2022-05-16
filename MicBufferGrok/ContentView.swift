49fa69bfa8b698abf9ab131ed338b361d743697e
🫐🥢 <blueberrychopsticks@pm.me>
2022-05-15 23:53:54 -0400

    Initial Commit

---
+++ MicBufferGrok/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MicBufferGrok
+//
+//  Created by laptop on 5/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
